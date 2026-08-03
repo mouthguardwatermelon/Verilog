4b83e1c9df655b74ad80334e82615f19752fb3ed
R Mukilan <125534610+mouthguardwatermelon@users.noreply.github.com>
2026-08-03 21:42:12 +0800

    Update counter.v
    fixed a small but conceptually big

--- counter.v
+++ counter.v
@@ -1,14 +1,15 @@
-module counter(
-input logic reset,
-input logic clk,
-input logic enable,
-input logic [3:0] counter
+module counter (
+    input  logic       reset,
+    input  logic       clk,
+    input  logic       enable,
+    output logic [3:0] count
 );
 
-always_ff @(posedge clk) begin
-    if (reset)
-    count <= 4'b0000;
-    else if (enable && counter < 4'b1010)
-    count <= count + 1'b1;
-end
+    always_ff @(posedge clk) begin
+        if (reset)
+            count <= 4'b0000;
+        else if (enable && count < 4'b1010)
+            count <= count + 1'b1;
+    end
+
 endmodule
